24d6997aab27f26a1663f77dbfbe9a012995c8b3
Is2lucy1 <lchung@unomaha.edu>
2021-12-04 15:17:26 -0700

    commit prior to adding sass and sassloader

--- subscriptions-app/src/App.vue
+++ subscriptions-app/src/App.vue
@@ -1,26 +1,83 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="header">
+        <nav class="navbar justify-content-between flex-nowrap flex-row">
+          <div class="container">
+            <h1><span>Subscriptions</span>App</h1>
+              <ul class="nav navbar-nav flex-row float-right">
+                <li class="nav-item">
+                  <router-link class="nav-link pr-3" to="/create">Add Subscription</router-link>
+                </li>
+                <li class="nav-item">
+                  <router-link class="nav-link" to="/">View Subscriptions</router-link>
+                </li>
+              </ul>
+          </div>
+        </nav>
+    </div>
+  <div class="main">
+    <List/>
+    <router-view class="modify"></router-view>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="scss">
+import List from './components/List.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    List
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Fira+Sans:wght@500&display=swap');
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Fira Sans', san-serif;
+
+  &::selection {
+    background: transparentize(#42B883, 0.5);
+  }
 }
+
+body {
+  background-color: #35495B;
+}
+
+a {
+  text-decoration: none;
+}
+
+header {
+  display: flex;
+  align-item: center;
+  justify-content: center;
+  padding: 10px 16px;
+  background-color: #2C3D4E;
+  box-shadow: 0px 0px 6px rgba(0, 0, 0, 0.1);
+
+  h1 {
+    color: #FFF;
+    font-size: 28px;
+
+    span {
+      color: #42B883;
+    }
+  }
+}
+
+h3 {
+  padding-top: 10px;
+}
+
+.modify {
+  padding: 10px;
+}
+
 </style>
+
---
+++ subscriptions-app/src/components/List.vue
@@ -0,0 +1,67 @@
+<template>
+
+  <div class="List">
+    <div class="row justify-content-center">
+        <div class="col-md-5">
+            <h3 class="text-center">Subscriptions</h3>
+              <table class="table align-middle">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Description</th>
+                        <th>Amount</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="subscription in subscriptions" :key="subscription.id">
+                        <td>{{ subscription.title }}</td>
+                        <td>{{ subscription.description }}</td>
+                        <td>{{ subscription.amount }}</td>
+<!--                        <td>-->
+<!--                            <router-link :to="{name: 'edit', params: { id: subscription.key }}" class="btn btn-primary">Edit-->
+<!--                            </router-link>-->
+<!--                            <button @click.prevent="deleteSubscription(subscription.key)" class="btn btn-danger">Delete</button>-->
+<!--                        </td>-->
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: "List",
+  data() {
+    return {
+      subscriptions: [
+        {
+          id: '1',
+          title: 'Netflix',
+          description: 'video/show streaming',
+          amount: 15.69
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+.table td,.table th {
+  padding: 12px 15px;
+  border: 1px solid #ddd;
+  text-align:center;
+}
+
+
+</style>
